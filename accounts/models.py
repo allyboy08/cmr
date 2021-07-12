378435e1e363c4b18d0cbcf7bf64d654a97a1059
allyboy08 <alexsassman65@gmail.com>
2021-07-12 14:59:51 +0200

    part 8

--- accounts/models.py
+++ accounts/models.py
@@ -11,6 +11,13 @@ class Customer(models.Model):
     def __str__(self):
         return self.name
 
+class Tag(models.Model):
+    name = models.CharField(max_length=200, null=True)
+    
+    
+    def __str__(self):
+        return self.name
+
 class Product(models.Model):
     CATEGORY = (
 			('Indoor', 'Indoor'),
@@ -19,9 +26,14 @@ class Product(models.Model):
     name = models.CharField(max_length=200, null=True)
     price = models.FloatField(null=True)
     category = models.CharField(max_length=200, null=True, choices=CATEGORY)
-    description = models.CharField(max_length=200, null=True)
+    description = models.CharField(max_length=200, null=True, blank=True)
     date_created = models.DateTimeField(auto_now_add=True)
+    tag = models.ManyToManyField(Tag)
     
+    def __str__(self):
+        return self.name
+
+
     
 class Order(models.Model):
     STATUS = (
@@ -30,7 +42,9 @@ class Order(models.Model):
 			('Delivered', 'Delivered'),
     )
     
-    # customer = 
-    # product = 
+    customer = models.ForeignKey(Customer, null= True, on_delete= models.SET_NULL)
+    product = models.ForeignKey(Product, null= True, on_delete= models.SET_NULL)
     date_created = models.DateTimeField(auto_now_add=True)
     status = models.CharField(max_length=200, null=True, choices=STATUS)
+    
+  
