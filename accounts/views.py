78daeae6f6d4794b31844692488ee81550383a2e
allyboy08 <alexsassman65@gmail.com>
2021-07-19 16:05:53 +0200

    part 17

--- accounts/views.py
+++ accounts/views.py
@@ -9,7 +9,7 @@ from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import Group
 # Create your views here.
 from .models import *
-from .forms import OrderForm, CreateUserForm
+from .forms import OrderForm, CreateUserForm, CustomerForm
 from .filters import OrderFilter
 from .decorators import unauthenticated_user, allowed_users, admin_only
 
@@ -25,6 +25,11 @@ def registerPage(request):
 
 			group = Group.objects.get(name='customer')
 			user.groups.add(group)
+			#Added username after video because of error returning customer name if not added
+			Customer.objects.create(
+				user=user,
+				name=user.username,
+				)
 
 			messages.success(request, 'Account was created for ' + username)
 
@@ -33,7 +38,6 @@ def registerPage(request):
 
 	context = {'form':form}
 	return render(request, 'accounts/register.html', context)
-
 
 @unauthenticated_user
 def loginPage(request):
@@ -78,10 +82,39 @@ def home(request):
 	return render(request, 'accounts/dashboard.html', context)
 
 
-
+@login_required(login_url='login')
+@allowed_users(allowed_roles=['customer'])
 def userPage(request):
-    context = {}
+    orders = request.user.customer.order_set.all()
+    total_orders = orders.count()
+    delivered = orders.filter(status='Delivered').count()
+    pending = orders.filter(status='Pending').count()
+    print('orders', orders)
+    
+    context = {'orders': orders,
+	'total_orders':total_orders,'delivered':delivered,
+	'pending':pending}
+    
     return render(request, 'accounts/user.html', context)
+
+
+@login_required(login_url='login')
+@allowed_users(allowed_roles=['customer'])
+def accountSettings(request):
+        customer = request.user.customer
+        form = CustomerForm(instance=customer)
+
+        if request.method == 'POST':
+            form = CustomerForm(request.POST, request.FILES,instance=customer)
+            if form.is_valid():
+                form.save()
+
+
+        context = {'form':form}
+        return render(request, 'accounts/account_settings.html', context)
+
+
+
 
 
 @login_required(login_url='login')
--- crm1/urls.py
+++ crm1/urls.py
@@ -16,7 +16,8 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path, include
-
+from django.conf.urls.static import static
+from django.conf import settings
 # from django.http import HttpResponse
 
 
@@ -25,3 +26,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('accounts.urls'))
 ]
+
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
