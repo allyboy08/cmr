c743fb476ea1b7e95cb0a9fb667779fb7d73e7b2
allyboy08 <alexsassman65@gmail.com>
2021-08-12 16:20:21 +0200

    testing

--- functional_tests/test_login.py
+++ functional_tests/test_login.py
@@ -26,34 +26,31 @@ class TestLogin(StaticLiveServerTestCase):
             'LOGIN'
         )
         
-    def test_sign_up_link_redirects_to_sign_up_page(self):
-        self.browser.get(self.live_server_url)
+    # def test_sign_up_link_redirects_to_sign_up_page(self):
+    #     self.browser.get(self.live_server_url)
         
-        # user request page for first time
-        register_url = self.live_server_url + reverse('register')
-        self.browser.find_element_by_tag_name('a').click()
-        self.assertEquals(
-            self.browser.current_url,
-            register_url
-        )
+        
+    #     register_url = self.live_server_url + reverse('register')
+    #     self.browser.find_element_by_tag_name('a').click()
+    #     self.assertEquals(
+    #         self.browser.current_url,
+    #         register_url
+    #     )
         
     def test_user_signs_up(self):
-        user1 = User(
-            username = 'testing',
-            email = 'testing@gmail.com',
-            password = 'password'
-            
-        )
+        self.send_keys('form-control input', 'dennis1')
+        self.send_keys('form-control input', 'bobross21')
         self.browser.get(self.live_server_url)
         
-        # login_url = self.live_server_url + reverse('register')
+        # login_url = self.live_server_url + reverse('home')
         # self.browser.find_element_by_tag_name('input').click()
-        # login_url = self.live_server_url + reverse('login')
+        # login_url = self.live_server_url + reverse('home')
+        
         self.browser.find_element_by_class_name('login_btn').click()
+        # form = self.browser.find_element_by_class_name('col-md-5')
         self.assertEquals(
-            
-            self.browser.find_element_by_id('messages').text,
-            'Account was created for testing'
+            form.find_element_by_tag_name('h5').text,
+            'CUSTOMERS:'
         )
         
     
