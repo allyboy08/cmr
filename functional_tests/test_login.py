0907394dd76b49ae19ac6040b477c97ddc894a9b
allyboy08 <alexsassman65@gmail.com>
2021-08-23 15:40:41 +0200

    test

--- functional_tests/test_login.py
+++ functional_tests/test_login.py
@@ -1,23 +1,23 @@
-from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from django.contrib.auth.models import User
+# from selenium import webdriver
+# from selenium.webdriver.common.keys import Keys
+# from selenium.webdriver.common.by import By
+# from selenium.webdriver.support.ui import WebDriverWait
+# from selenium.webdriver.support import expected_conditions as EC
+# from django.contrib.auth.models import User
 
-from django.contrib.staticfiles.testing import StaticLiveServerTestCase
-from django.urls import reverse
-import time
+# from django.contrib.staticfiles.testing import StaticLiveServerTestCase
+# from django.urls import reverse
+# import time
 
 
-class TestLogin(StaticLiveServerTestCase):
+# class TestLogin(StaticLiveServerTestCase):
     
-    def setUp(self):
-        self.browser = webdriver.Chrome('/Users/allyboy08/Downloads/chromedriver_win32/chromedriver')
-        # self.browser.get("http://127.0.0.1:8000/login/?next=/")
+#     def setUp(self):
+#         self.browser = webdriver.Chrome('/Users/allyboy08/Downloads/chromedriver_win32/chromedriver')
         
-    def tearDown(self):
-        self.browser.close()
+        
+#     def tearDown(self):
+#         self.browser.close()
         
     
         
@@ -85,31 +85,27 @@ class TestLogin(StaticLiveServerTestCase):
         # self.browser.find_element_by_id("user-registration-submit").click()
         # self.assertEqual(username, self.browser.find_element_by_id("username-text").text)
         
-    def test_user_is_redirected_to_register_page(self):
+    # def test_user_is_redirected_to_register_page(self):
         
-        self.browser.get(self.live_server_url)
-        register_url = self.live_server_url + reverse('register')
-        self.browser.find_element_by_tag_name('a').click()
-        self.assertEquals(
-            self.browser.current_url,
-            register_url
-        )
+    #     self.browser.get(self.live_server_url)
+    #     register_url = self.live_server_url + reverse('register')
+    #     self.browser.find_element_by_tag_name('a').click()
+    #     self.assertEquals(
+    #         self.browser.current_url,
+    #         register_url
+    #     )
     
-        # user = User(
-        # username = "okay2",
-        # email = "testing4@gmail.com",
-        # password = "working2"
-        # )
+        
        
         
-        self.browser.find_element_by_id("id_username").send_keys("okay1"),
-        self.browser.find_element_by_id("id_email").send_keys("testing4@gmail.com"),
-        self.browser.find_element_by_id("id_password1").send_keys("working2"),
-        self.browser.find_element_by_id("id_password2").send_keys("working2")
+    #     self.browser.find_element_by_id("id_username").send_keys("okay1"),
+    #     self.browser.find_element_by_id("id_email").send_keys("testing4@gmail.com"),
+    #     self.browser.find_element_by_id("id_password1").send_keys("working2"),
+    #     self.browser.find_element_by_id("id_password2").send_keys("working2")
         
         
-        submit_btn = self.browser.find_element_by_class_name('login_btn')
-        submit_btn.click()
+    #     submit_btn = self.browser.find_element_by_class_name('login_btn')
+    #     submit_btn.click()
         # login_url = self.live_server_url + reverse('login')
         
         # login_url = self.live_server_url + reverse('login')
@@ -129,4 +125,69 @@ class TestLogin(StaticLiveServerTestCase):
     
         
     
+from django.test.testcases import TransactionTestCase
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from accounts.models import Customer
+from accounts.forms import OrderForm, CreateUserForm, CustomerForm
+from django.contrib.auth.models import User
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
+from django.urls import reverse
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import time
+
+
+class TestLogin(TransactionTestCase):
+    def setUp(self):
+        self.browser = webdriver.Chrome("/Users/allyboy08/Downloads/chromedriver_win32/chromedriver")
+        self.browser.get("http://127.0.0.1:8000/")
+        
+    
+    def test_register(self):
+        self.regLink = self.browser.find_element_by_class_name("ml-2")
+        
+        self.regLink.click()
+        try:
+            self.username = WebDriverWait(self.browser, 50).until(EC.presence_of_element_located((By.ID, "id_username")))
+            self.username.send_keys("okay2")
+            self.email = self.browser.find_element_by_id("id_email")
+            self.email.send_keys("testing5@gmail.com")
+            self.password1 = self.browser.find_element_by_id("id_password1")
+            self.password1.send_keys("working3")
+            self.password2 = self.browser.find_element_by_id("id_password2")
+            self.password2.send_keys("working3")
+            
+        except Exception as err:
+            print(err)
+            self.browser.quit()
+        
+        try:
+            self.registerButton = self.browser.find_element_by_class_name("btn")
+            self.registerButton.click()
+        except Exception as err:
+            print(err)
+        
+        # time.sleep(20)
+    
+
+        username_input = self.browser.find_element_by_name('username')
+        password_input = self.browser.find_element_by_name('password')
+        
+        
+        submit_btn = self.browser.find_element_by_class_name('login_btn')
+        
+        
+        username_input.send_keys('okay2')
+        password_input.send_keys('working3')
+        
+        
+       
+        submit_btn.click()
+        
+        time.sleep(5)
+        
+        
     
